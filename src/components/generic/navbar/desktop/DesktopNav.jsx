af83fc0cc4f36951c10310a118b9f1d19627b81e
3103ski <bjastski@gmail.com>
2022-07-06 08:53:26 -0700

    updates to mobile

--- src/components/generic/navbar/desktop/DesktopNav.jsx
+++ src/components/generic/navbar/desktop/DesktopNav.jsx
@@ -1,6 +1,6 @@
 // --> React
 import React from 'react';
-import { Link } from 'react-router-dom';
+// import { Link } from 'react-router-dom';
 import { useLocation } from 'react-router-dom';
 
 // --> Packages

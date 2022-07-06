a416c66f9e0ed2400599f1629fb4af35be1cfe1b
3103ski <bjastski@gmail.com>
2022-07-06 09:52:40 -0700

    mobile updates to homepage

--- src/hooks/useRatio.js
+++ src/hooks/useRatio.js
@@ -1,5 +1,7 @@
 // --> React
 import React from 'react';
+import SASS from '../sass/index.scss';
+import { intFromPx } from 'util';
 
 export default function useRatio(ratio) {
 	const [width, setWidth] = React.useState(0);
@@ -7,6 +9,7 @@ export default function useRatio(ratio) {
 
 	const calculateHeight = React.useCallback(() => {
 		let multiplier = 0;
+		let screen = window.innerWidth;
 
 		switch (ratio) {
 			case '16:9':
@@ -15,8 +18,16 @@ export default function useRatio(ratio) {
 				break;
 		}
 
-		let width = window.innerWidth;
-		let height = width * multiplier;
+		let width;
+		let height;
+		// if (screen < intFromPx(SASS.bp_mobile_lg)) {
+		// 	width = window.innerWidth;
+		// 	height = width * 1.7777777;
+		// } else {
+		// }
+		width = window.innerWidth;
+		height = width * multiplier;
+
 		setWidth(width);
 		setHeight(height);
 	}, [ratio]);
